be8b558fb5840525f2481e55a1435e1cfaba8b5c
Dasha Sedova <dashassed@gmail.com>
2023-06-07 21:09:02 +0300

    fix: tests1.2

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,18 +1,18 @@
 const jwtAuth = require('../utils/jwtAuth');
-const errors = require('../errors/errors');
+const UnauthorizedError = require('../errors/unauthorized');
 
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
 
   if (!authorization) {
-    return res.status(errors.unauthorized).send({ message: 'Требуется авторизация' });
+    return next(new UnauthorizedError('Требуется авторизация'));
   }
   const token = authorization.replace('Bearer ', '');
   let payload;
   try {
     payload = jwtAuth.checkToken(token);
   } catch (err) {
-    return res.status(errors.unauthorized).send({ message: 'Требуется авторизация' });
+    return next(new UnauthorizedError('Требуется авторизация'));
   }
   req.user = payload;
   return next();
